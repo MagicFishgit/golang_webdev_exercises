57df43f004573e4e7b0b979b8c49d9e44ce90593
MagicFishgit <rudivisagie@icloud.com>
2020-09-26 12:45:41 +0200

    added ex06- Don't really care about this one.

---
+++ exercises_4/ex_06/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func serve(c net.Conn) {
+	//Read from connection.
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		if ln == "" { //end of HTTP Request Headers
+			break
+		}
+		fmt.Println(ln)
+	}
+}
+
+func main() {
+
+	//I create a listener on port 8080 and specify to the net package that it should be tc.
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Error listening on port.", err)
+	}
+	defer listener.Close() //I need to remember to use defer to close things. Defer runs whatever you give it at the end of the funciton.
+
+	//ive created the connection now I need to specify what it does with incomming connections.
+	for {
+		conn, err := listener.Accept() //accept the connection
+		if err != nil {
+			log.Println(err) //not using fatalln because I dont want the program to end if it got a failed connection.
+			continue
+		}
+
+		//Serve the connection
+		go serve(conn)
+
+		//write to the connection
+		io.WriteString(conn, "Hey there buddy! You are connected.")
+
+		defer conn.Close()
+
+		fmt.Println("Code got here")
+	}
+
+}
